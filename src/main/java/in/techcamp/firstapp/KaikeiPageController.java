d74dd0d27e4869cb3e3bc9e284afee578b76e350
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-30 20:20:41 +0900

    ページ遷移

--- src/main/java/in/techcamp/firstapp/KaikeiPageController.java
+++ src/main/java/in/techcamp/firstapp/KaikeiPageController.java
@@ -9,4 +9,34 @@ public class KaikeiPageController {
     public String showTop(){
         return "kaikei";
     }
+
+    @GetMapping("/kaikei/group")
+    public String showGroup(){
+        return "group";
+    }
+
+    @GetMapping("/kaikei/groupForm")
+    public String showGroupForm(){
+        return "groupForm";
+    }
+
+    @GetMapping("/kaikei/finbook")
+    public String showFinbook(){
+        return "finbook";
+    }
+
+    @GetMapping("/kaikei/finbookForm")
+    public String showFinbookForm(){
+        return "finbookForm";
+    }
+
+    @GetMapping("/kaikei/finsheet")
+    public String showFinsheet(){
+        return "finsheet";
+    }
+
+    @GetMapping("/kaikei/finsheetForm")
+    public String showFinsheetForm(){
+        return "finsheetForm";
+    }
 }
