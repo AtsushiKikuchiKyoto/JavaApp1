8bc77f58fca6c572fdfe44478996a87d85a9d26c
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2025-01-03 11:01:13 +0900

    団体情報ページへ会計選択追加

--- src/main/java/in/techcamp/firstapp/KaikeiFinbookController.java
+++ src/main/java/in/techcamp/firstapp/KaikeiFinbookController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,8 +21,15 @@ public class KaikeiFinbookController {
 
     @GetMapping("/kaikei/finbook/{id}")
     public String showGroupById(@PathVariable("id") Long id, Model model){
-        var group = kaikeiGroupRepository.findById(id);
-        model.addAttribute("group", group);
+        try {
+            var group = kaikeiGroupRepository.findById(id);
+            model.addAttribute("group", group);
+            var finbookList = kaikeiFinbookRepository.findByGroupId(id);
+            model.addAttribute("finbookList", finbookList);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+        }
         return "finbook";
     }
 
--- src/main/java/in/techcamp/firstapp/KaikeiFinbookRepository.java
+++ src/main/java/in/techcamp/firstapp/KaikeiFinbookRepository.java
@@ -2,11 +2,17 @@ package in.techcamp.firstapp;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Mapper
 public interface KaikeiFinbookRepository {
     @Insert("INSERT INTO finbookDemo (name, fiscal_year, start_date, end_date, group_id) VALUES (#{name}, #{fiscalYear}, #{startDate}, #{endDate}, #{groupId})")
     void insert(String name , long fiscalYear , LocalDate startDate , LocalDate endDate , long groupId );
+
+    @Select("SELECT * FROM finbookDemo WHERE group_id = #{id}")
+    List<KaikeiFinbookEntity> findByGroupId(@Param("id") Long id);
 }
